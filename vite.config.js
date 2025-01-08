0766eafaaaf053581c99cfad5252692190081181
taimoormirza1999 <taimoorhamza1999@gmail.com>
2025-01-09 02:25:40 +0400

    update vite connfig

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-})
+  base: '/', // Keep this only if deploying in a root domain
+  build: {
+    outDir: 'dist', // Vite default output, ensure it matches
+  },
+});
